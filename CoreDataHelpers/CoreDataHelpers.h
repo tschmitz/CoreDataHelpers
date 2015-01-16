0b23c5c5b34328ac55e2077d9136e4f5b4f97c53
Tim Schmitz <tschmitz@gmail.com>
2015-01-16 12:32:19 -0500

    Initial Commit

---
+++ CoreDataHelpers/CoreDataHelpers.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataHelpers.h
+//  CoreDataHelpers
+//
+//  Created by Tim Schmitz on 1/16/15.
+//  Copyright (c) 2015 Tap and Tonic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataHelpers.
+FOUNDATION_EXPORT double CoreDataHelpersVersionNumber;
+
+//! Project version string for CoreDataHelpers.
+FOUNDATION_EXPORT const unsigned char CoreDataHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataHelpers/PublicHeader.h>
+
+
